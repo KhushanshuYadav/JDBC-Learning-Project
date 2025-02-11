70a279ad3fdc183c3e5447e8b52e3b53dfca59ca
KhushanshuYadav <khushanshuyadav2@gmail.com>
2025-02-11 12:57:22 +0530

    2.Created class for connection

--- StudentManagementSytem/src/Start.java
+++ StudentManagementSytem/src/Start.java
@@ -1,9 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 
 public class Start {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("");
+		
+		System.out.println("Hello,");
+		
+		 BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		
+		
+		
+		while(true) {
+			
+			System.out.println("Enter 1 to add student details");
+			System.out.println("Enter 2 to delete student details");
+			System.out.println("Enter 3 to update student details");
+			System.out.println("Enter 4 to end opeartions");
+			
+			int c= Integer.parseInt(bReader.readLine());
+			
+			
+			if(c==1) {
+				//TODO add student
+				
+			}
+			else if(c==2) {
+				//TODO delete student
+				
+			}
+			else if(c==3) {
+				//TODO update student
+				
+			}
+			else if(c==4) {
+				break;
+				
+			}
+			else {
+				System.out.println("Enter a valid value");
+				continue;
+				
+				
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
---
+++ StudentManagementSytem/src/com/student/manage/CreateConnection.java
@@ -0,0 +1,44 @@
+package com.student.manage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CreateConnection {
+	
+	static Connection connection=null;
+	
+	public static Connection giveConnection() {
+		
+		
+		
+		//1 Load the driver
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		    System.out.println("Driver added");
+		    //In newer versions driver is loaded automatically no need to load explicitly
+		    }
+		catch(ClassNotFoundException e){
+			e.printStackTrace();
+			}
+		
+		//2 Create connection 
+		
+		
+		final String url="jdbc:mysql://localhost:3306/students";
+		final String user="root";
+		final String password="123456789";
+		
+		try {
+			connection=DriverManager.getConnection(url, user, password);
+			System.out.println("Connection Sucessfull");
+			}
+		catch(SQLException e) {
+			System.out.println(e.getMessage());
+			}
+		
+		return connection;
+		
+	}
+
+}
